d01d4c263ed0b65da762499e5cb928ae1c860889
williamfisher9 <fisher.a.william@gmail.com>
2022-02-14 12:38:33 -0600

    adding login compoenent to the skeleton

--- frontend/src/components/AppMainRouter/AppMainRouter.js
+++ frontend/src/components/AppMainRouter/AppMainRouter.js
@@ -6,19 +6,22 @@ import CouponsTable from "../CouponsTable/CouponsTable";
 import SettingsTable from "../SettingsTable/SettingsTable";
 import AdminItemsViewer from "../ItemsViewer/AdminItemsViewer";
 import AppOrders from "../AppOrders/AppOrders";
+import Login from "../Login/Login";
 
 class AppMainRouter extends Component {
   render() {
     return (
       <Router>
         <Routes>
-          <Route exact path="/" element={<Client />} />
+          <Route exact path="/" element={<Login />} />
+
           <Route exact path="/admin" element={<Admin />} />
           <Route exact path="/adminCoupons" element={<><Admin /><CouponsTable/></>} />
           <Route exact path="/adminSettings" element={<><Admin /><SettingsTable/></>} />
           <Route exact path="/adminItems" element={<><Admin /><AdminItemsViewer/></>} />
           <Route exact path="/adminOrders" element={<><Admin /><AppOrders/></>} />
 
+          <Route exact path="/client" element={<Client />} />
           <Route exact path="/clientItems" element={<><Client /></>} />
           <Route exact path="/clientOrders" element={<><Client /></>} />
           <Route exact path="/clientCart" element={<><Client /></>} />
