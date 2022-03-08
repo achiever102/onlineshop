e91fc14129577440d1f61c022f58e41d9821889f
williamfisher9 <fisher.a.william@gmail.com>
2022-03-08 14:08:12 -0600

    app.js frontend change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ class App extends Component {
   render(){
     return (
       <div>
+        <h1></h1>
+        <h1></h1>
+        <h1></h1>
+        <h1></h1>
         <AppMainRouter />
       </div>
     )
