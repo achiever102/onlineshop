d6081ff6d4cb1c3f84ad3f51c99ce9b61a504254
achiever102 <98985292+achiever102@users.noreply.github.com>
2022-03-28 09:55:51 -0500

    Delete src directory

--- src/main/java/com/achievers/onlineshop/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.achievers.onlineshop.repository;
-
-import com.achievers.onlineshop.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
--- src/main/java/com/achievers/onlineshop/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.achievers.onlineshop.repository;
-
-import com.achievers.onlineshop.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
