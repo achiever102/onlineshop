58d354bafdd55abf96ecaa254bbdd74a1286182b
williamfisher9 <fisher.a.william@gmail.com>
2022-04-16 05:52:19 -0500

    update menu items

--- frontend/src/components/AppFooter/AppFooter.js
+++ frontend/src/components/AppFooter/AppFooter.js
@@ -4,6 +4,10 @@ import { Col, Row, Container } from "react-bootstrap";
 import AppNewsletter from "../AppNewsletter/AppNewsletter";
 import styled from 'styled-components';
 
+import {BsFacebook, BsInstagram} from 'react-icons/bs';
+import {FiTwitter} from 'react-icons/fi';
+import {AiOutlinePhone, AiOutlineMail} from 'react-icons/ai';
+
 const StyledDiv = styled.div`
     bottom: 0;
     position: relative;
@@ -21,17 +25,33 @@ class AppFooter extends Component {
          <Container>
            <StyledDiv>
          <Row className="text-white">
-              <Col sm={12} md={4} lg={3} className="text-center"><h5><u>CONTACT</u></h5></Col>
-              <Col sm={12} md={4} lg={3} className="text-center"><h5><u>CUSTOMER SERVICE</u></h5></Col>
-              <Col sm={12} md={4} lg={3} className="text-center"><h5><u>CONTACT</u></h5></Col>
-              <Col sm={12} md={4} lg={3} className="text-center"><h5><u>CONTACT</u></h5></Col>
+              <Col sm={12} md={4} lg={4} className="text-left"><h5><u>CONTACT</u></h5></Col>
+              <Col sm={12} md={4} lg={4} className="text-left"><h5><u>CUSTOMER SERVICE</u></h5></Col>
+              <Col sm={12} md={4} lg={4} className="d-flex justify-content-start"><h5><u>Help & Policies</u></h5></Col>
           </Row>
+
+          <Row className="text-white">
+              <Col sm={12} md={4} lg={4} className="text-left"><p className="text-white" style={{cursor: "pointer"}}><BsFacebook /> Facebook</p></Col>
+              <Col sm={12} md={4} lg={4} className="text-left"><p><AiOutlinePhone /> 1-800-888-8888</p></Col>
+              <Col sm={12} md={4} lg={4} className="d-flex justify-content-start"><p>Terms and Conditions</p></Col>
+          </Row>
+          <Row className="text-white">
+              <Col sm={12} md={4} lg={4} className="text-left"><p className="text-white" style={{cursor: "pointer"}}><FiTwitter /> Twitter</p></Col>
+              <Col sm={12} md={4} lg={4} className="text-left"><p><AiOutlineMail /> Sales: ludos-sales@ludos.com</p></Col>
+              <Col sm={12} md={4} lg={4} className="d-flex justify-content-start"><p>Terms of Use</p></Col>
+          </Row>
+          <Row className="text-white">
+              <Col sm={12} md={4} lg={4} className="text-left"><p className="text-white" style={{cursor: "pointer"}}><BsInstagram /> Instagram</p></Col>
+              <Col sm={12} md={4} lg={4} className="text-left"><p><AiOutlineMail /> Support: ludos-support@ludos.com</p></Col>
+              <Col sm={12} md={4} lg={4} className="d-flex justify-content-start"><p>Copyrights</p></Col>
+          </Row>
+
           <Row className="mt-5">
             <Col className="text-center text-white">© 2022 LUDOS FOR VIDEO GAMES</Col>
           </Row>
           </StyledDiv>
          </Container>
-        
+
       </div>
       </div>
     );
--- frontend/src/components/Client/Checkout/SuccessfulOrder.js
+++ frontend/src/components/Client/Checkout/SuccessfulOrder.js
@@ -1,16 +1,21 @@
 import React, {useContext} from 'react';
 
 import AuthContext from "../../../context/AuthContext";
+import AppFooter from '../../AppFooter/AppFooter';
 
 export default function SuccessfulOrder(){
 
     const { licensesFileUrl } = useContext(AuthContext);
 
     return (
+        <div>
         <div className='container bg-light text-center mt-5 p-5 border' style={{borderRadius: "10px"}}>
             <h3>Thank you! Your order was placed successfully</h3>
             <h4 className='mt-3'><b>Click <a href={licensesFileUrl} className="link-success">this link</a> to download your licenses</b></h4>
             <h4 className='mt-3'>You can also download you licenses anytime from Orders page</h4>
+        </div>
+
+        <AppFooter />
         </div>
     )
 
